6274fdf8f7027f419777154db92f3d0703978a65
Barbara Rodrigues <barbara.rodrigues@cabify.com>
2023-04-15 00:04:29 +0200

    astrofeed with bg

--- src/views/Feeds/AstroFeed/AstrologicalFeed.jsx
+++ src/views/Feeds/AstroFeed/AstrologicalFeed.jsx
@@ -1,77 +1,35 @@
 import { NextUIProvider, Card, Grid, Text, Link, Button, Col, Row } from '@nextui-org/react'
 import { AppBack } from '../../../components/BackgroundSigns/Background'
-
+import "./AstroFeed.css"
 const AstroFeed = () => {
     return (
 
         <NextUIProvider>
-            <Card css={{ p: "$6", mw: "400px", h: "400px" }}>
-                <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+            <div className="background-wrapper">
+                <AppBack />
+            </div>
+            <div style={{ textAlign: 'center' }}>
+                <Grid.Container>
+                    <Grid xs={12} sm={6} md={4}>
+                        <Card css={{ p: "$6", mw: "400px", h: "400px", backgroundColor: "transparent" }}>
+                            <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
 
-                    <Text h3 color="black">
-                        Daily Horoscope
-                    </Text>
+                                <Text h3 color="black">
+                                    Daily Horoscope
+                                </Text>
 
-                </Card.Header>
-                <Card.Body css={{ p: 0 }}>
-                    <Card.Image
-                        src="https://phantom-marca.unidadeditorial.es/80fca989dae1f5722a4edf9c7ff6075c/crop/0x0/1320x743/resize/1320/f/jpg/assets/multimedia/imagenes/2022/04/19/16503614614344.jpg"
-                        width="100%"
-                        height="100%"
-                        objectFit="cover"
-                        alt="Card horoscope daily"
-                    />
-                </Card.Body>
+                            </Card.Header>
+                            <Card.Body css={{ p: 0 }}>
+                                <Card.Image
+                                    src="https://phantom-marca.unidadeditorial.es/80fca989dae1f5722a4edf9c7ff6075c/crop/0x0/1320x743/resize/1320/f/jpg/assets/multimedia/imagenes/2022/04/19/16503614614344.jpg"
+                                    width="100%"
+                                    height="100%"
+                                    objectFit="cover"
+                                    alt="Card horoscope daily"
+                                />
+                            </Card.Body>
 
-                {/*
-
-                <Card.Body css={{ py: "$2" }}>
-                    <Text>
-                        <h1> Social Feed 1 </h1>
-                    </Text>
-                </Card.Body>
-    */}
-                <Card.Footer isBlurred
-                    css={{
-                        position: "absolute",
-                        bgBlur: "#ffffff66",
-                        borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
-                        bottom: 0,
-                        zIndex: 1,
-                    }}>
-
-
-                    <Button
-                        icon
-                        color="dark"
-                        target="_blank"
-
-                    >
-                        Get your Sign Today
-                    </Button>
-
-                </Card.Footer>
-            </Card>
-
-            <Card css={{ p: "$6", mw: "400px", h: "400px" }}>
-                <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
-
-                    <Text h3 color="black">
-                        Daily Tarot
-                    </Text>
-
-                </Card.Header>
-                <Card.Body css={{ p: 0 }}>
-                    <Card.Image
-                        src="https://thumbs.dreamstime.com/b/mystical-banner-astrology-tarot-boho-design-universe-art-golden-crescent-sun-black-background-clouds-esoteric-200267643.jpg"
-                        width="100%"
-                        height="100%"
-                        objectFit="cover"
-                        alt="Card Tarot daily"
-                    />
-                </Card.Body>
-
-                {/*
+                            {/*
 
                 <Card.Body css={{ py: "$2" }}>
                     <Text>
@@ -79,28 +37,80 @@ const AstroFeed = () => {
                     </Text>
                 </Card.Body>
     */}
-                <Card.Footer isBlurred
-                    css={{
-                        position: "absolute",
-                        bgBlur: "#ffffff66",
-                        borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
-                        bottom: 0,
-                        zIndex: 1,
-                    }}>
+                            <Card.Footer isBlurred
+                                css={{
+                                    position: "absolute",
+                                    bgBlur: "#ffffff66",
+                                    borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
+                                    bottom: 0,
+                                    zIndex: 1,
+                                }}>
 
 
-                    <Button
-                        icon
-                        color="dark"
-                        target="_blank"
+                                <Button
+                                    icon
+                                    color="dark"
+                                    target="_blank"
 
-                    >
-                        Get your Tarot Today
-                    </Button>
+                                >
+                                    Get your Sign Today
+                                </Button>
 
-                </Card.Footer>
-            </Card>
+                            </Card.Footer>
+                        </Card>
+                    </Grid>
+                    <Grid xs={12} sm={6} md={4}>
 
+                        <Card css={{ p: "$6", mw: "400px", h: "400px", backgroundColor: "transparent" }}>
+                            <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+
+                                <Text h3 color="black">
+                                    Daily Tarot
+                                </Text>
+
+                            </Card.Header>
+                            <Card.Body css={{ p: 0 }}>
+                                <Card.Image
+                                    src="https://thumbs.dreamstime.com/b/mystical-banner-astrology-tarot-boho-design-universe-art-golden-crescent-sun-black-background-clouds-esoteric-200267643.jpg"
+                                    width="100%"
+                                    height="100%"
+                                    objectFit="cover"
+                                    alt="Card Tarot daily"
+                                />
+                            </Card.Body>
+
+                            {/*
+
+                <Card.Body css={{ py: "$2" }}>
+                    <Text>
+                        <h1> Social Feed 1 </h1>
+                    </Text>
+                </Card.Body>
+    */}
+                            <Card.Footer isBlurred
+                                css={{
+                                    position: "absolute",
+                                    bgBlur: "#ffffff66",
+                                    borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
+                                    bottom: 0,
+                                    zIndex: 1,
+                                }}>
+
+
+                                <Button
+                                    icon
+                                    color="dark"
+                                    target="_blank"
+
+                                >
+                                    Get your Tarot Today
+                                </Button>
+
+                            </Card.Footer>
+                        </Card>
+                    </Grid>
+                </Grid.Container>
+            </div>
         </NextUIProvider >
 
 
