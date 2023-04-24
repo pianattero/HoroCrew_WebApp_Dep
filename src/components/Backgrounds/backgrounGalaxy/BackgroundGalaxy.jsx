5f818ae7b5e152f06181033f2c9be2892fc8e1c9
pianattero <mariapianattero@gmail.com>
2023-04-24 14:21:27 +0200

    delete starfield dependency

--- src/components/Backgrounds/backgrounGalaxy/BackgroundGalaxy.jsx
+++ src/components/Backgrounds/backgrounGalaxy/BackgroundGalaxy.jsx
@@ -1,21 +1,19 @@
-import React, { Component } from 'react'
-
-
-import StarfieldAnimation from 'react-starfield-animation'
+import React, { Component } from "react";
 
 class Galaxy extends Component {
-    render() {
-        return (
-            <StarfieldAnimation
-                style={{
-                    position: 'absolute',
-                    width: '100%',
-                    height: '100%',
-                    backgroundImage: "url(https://c1.wallpaperflare.com/preview/766/728/849/black-blue-gray-night.jpg)",
-                    backgroundSize: "cover"
-                }}
-            />
-        )
-    }
+  render() {
+    return (
+      <div
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          backgroundImage:
+            "url(https://c1.wallpaperflare.com/preview/766/728/849/black-blue-gray-night.jpg)",
+          backgroundSize: "cover",
+        }}
+      ></div>
+    );
+  }
 }
 export default Galaxy;
