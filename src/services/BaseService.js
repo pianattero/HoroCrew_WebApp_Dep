188c477136eea3ca73ad275981d49794e51699c2
pianattero <mariapianattero@gmail.com>
2023-04-24 14:38:06 +0200

    config netlify

--- src/services/BaseService.js
+++ src/services/BaseService.js
@@ -1,37 +1,39 @@
-import axios from 'axios';
-import { getAccessToken, logout } from '../stores/AccessTokenStore';
+import axios from "axios";
+import { getAccessToken, logout } from "../stores/AccessTokenStore";
 
 const INVALID_STATUS_CODES = [401];
 
 export const createHttp = (useAccessToken = false) => {
-    const http = axios.create({
-        baseURL: 'http://localhost:3000',
-    })
+  const http = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+  });
 
-    http.interceptors.request.use(
-        (config) => {
-            if (useAccessToken && getAccessToken()) {
-                config.headers.Authorization = `Bearer ${getAccessToken()}`
-            }
+  http.interceptors.request.use(
+    (config) => {
+      if (useAccessToken && getAccessToken()) {
+        config.headers.Authorization = `Bearer ${getAccessToken()}`;
+      }
 
-            return config
-        },
-        err => Promise.reject(err)
-    )
+      return config;
+    },
+    (err) => Promise.reject(err)
+  );
 
-    http.interceptors.response.use(
-        (response) => response.data,
-        (error) => {
-
-            if (error?.response?.status && INVALID_STATUS_CODES.includes(error.response.status)) {
-                if (getAccessToken()) {
-                    logout()
-                }
-            }
-
-            return Promise.reject(error)
+  http.interceptors.response.use(
+    (response) => response.data,
+    (error) => {
+      if (
+        error?.response?.status &&
+        INVALID_STATUS_CODES.includes(error.response.status)
+      ) {
+        if (getAccessToken()) {
+          logout();
         }
-    )
+      }
 
-    return http;
-}
+      return Promise.reject(error);
+    }
+  );
+
+  return http;
+};
